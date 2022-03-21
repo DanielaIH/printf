f0d311f4e79653c54d832188df2f2caf10068a3f
Robert_octavo <robert.ortega.octavo@gmail.com>
2022-03-21 15:57:02 -0500

    test

--- _printf.c
+++ _printf.c
@@ -35,8 +35,7 @@ while (format != NULL && format[i] != '\0')
 	} /*end else*/
 	else /*Others chars in format*/
 	{
-		_putchar(format[i]);
-		counter++;
+		_putchar(format[i]), counter++;
 	} i++; } /*End While*/
 va_end(args);
 return (counter);
