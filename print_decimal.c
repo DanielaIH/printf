f2ac1b6a1ca8cf9693f8775d3440f93724871b87
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-03-21 19:28:43 -0500

    Update print decimal

--- print_decimal.c
+++ print_decimal.c
@@ -7,5 +7,19 @@
 
 int p_decimal(va_list valist)
 {
-	
+	int n = va_arg(valist, int);
+	int ultimo = n % 10;
+	int base = 1;
+	int cantidad = 1;
+	int numero, digito;
+	n = n / 10;
+	numero = n;
+	if (ultimo < 0)
+	{
+		_putchar('-');
+		numero = numero * - 1;
+		n = n * - 1;
+		ultimo = ultimo * - 1;
+		cantidad++;
+	}
 }
